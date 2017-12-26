2136fcb62307ea77c22f570150a948b605224b39
tsorrels <tsorrels@github.com>
2017-12-26 12:50:22 -0800

    added check encryption unit test

--- test/test_wifi_parser.py
+++ test/test_wifi_parser.py
@@ -16,12 +16,38 @@ class TestWifiParser(unittest.TestCase):
         hex_string = fd.read()
         hex_string = hex_string.strip() #remove whitespace from string
         hex_data = (str(hex_string)).decode('hex')
-        expected_src_mac = '0c:8b:fd:75:a7:38'
-        expected_dst_mac = '00:00:ca:11:22:33'
+        expected_src_mac = '00:08:ca:f7:72:ca'
+        expected_dst_mac = 'f4:3e:9d:03:63:95'
+        expected_bssid = '74:85:2a:4d:9c:aa'
+        
+        wifi_header = wifi_parser.parse_header(hex_data)
+
+        self.assertEqual(expected_bssid, wifi_header.w_header.addr1) 
+        self.assertEqual(expected_src_mac, wifi_header.w_header.addr2) 
+        self.assertEqual(expected_dst_mac, wifi_header.w_header.addr3) 
+
+
+    def test_wifi_header_encryted(self):
+        wifi_parser = WifiParser()
+        
+        fd = open('./test/test_data/wifi_frame_encrypted_hex_string.txt')
+        hex_string = fd.read()
+        hex_string = hex_string.strip() #remove whitespace from string
+        hex_data = (str(hex_string)).decode('hex')
 
         wifi_header = wifi_parser.parse_header(hex_data)
 
-        print wifi_header.w_header.addr1
-        print wifi_header.w_header.addr2
-        print wifi_header.w_header.addr3
-        print wifi_header.w_header.addr4
+        self.assertEqual(1, wifi_header.w_header.protected)
+
+    def test_wifi_header_not_encryted(self):
+        wifi_parser = WifiParser()
+
+        fd = open('./test/test_data/wifi_frame_tcp_hex_string.txt')        
+        hex_string = fd.read()
+        hex_string = hex_string.strip() #remove whitespace from string
+        hex_data = (str(hex_string)).decode('hex')
+
+        wifi_header = wifi_parser.parse_header(hex_data)
+
+        self.assertEqual(0, wifi_header.w_header.protected)
+        
